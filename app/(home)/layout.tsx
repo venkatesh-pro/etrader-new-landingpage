d8045c009967397726b6698cfe329a1a07028b5b
venkatesh k <programervenkatesh@gmail.com>
2025-02-22 14:28:47 +0530

    added chatbot

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../globals.css";
 import "./home.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "../fonts/Universal-Sans-Display-350.ttf",
@@ -21,7 +22,57 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.className} antialiased`}>{children}</body>
+      <body className={`${geistSans.className} antialiased`}>
+        {children}
+
+        <Script
+          id="openwidget-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.__ow = window.__ow || {};
+              window.__ow.organizationId = "da59421c-7f39-4052-88cf-7b7eebfdc8cd";
+              window.__ow.template_id = "573d9bcf-30ec-49f6-b03b-b1006c3ff8e7";
+              window.__ow.integration_name = "manual_settings";
+              window.__ow.product_name = "chatbot";
+              (function(n,t,c){
+                function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}
+                var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},
+                once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},
+                get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");
+                return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},
+                init:function(){
+                  var n=t.createElement("script");
+                  n.async=!0,n.type="text/javascript",
+                  n.src="https://cdn.openwidget.com/openwidget.js",
+                  t.head.appendChild(n)
+                }};
+                !n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e
+              }(window,document,[].slice));
+            `,
+          }}
+        />
+
+        {/* Fallback for NoScript */}
+        <noscript>
+          You need to{" "}
+          <a
+            href="https://www.chatbot.com/help/chat-widget/enable-javascript-in-your-browser/"
+            rel="noopener nofollow"
+          >
+            enable JavaScript
+          </a>{" "}
+          in order to use the AI chatbot tool powered by{" "}
+          <a
+            href="https://www.chatbot.com/"
+            rel="noopener nofollow"
+            target="_blank"
+          >
+            ChatBot
+          </a>
+          .
+        </noscript>
+      </body>
     </html>
   );
 }
