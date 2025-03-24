6e889e8bf402db71a51c9f29e79afb678374d7cb
venkatesh k <venkateshk@venkateshs-MacBook-Pro.local>
2025-03-24 15:04:50 +0530

    completed the red border for the input

--- components/InputField/InputField.tsx
+++ components/InputField/InputField.tsx
@@ -34,9 +34,9 @@ const InputField = ({
         } border border-[#c4c4c4] text-[17px] font-normal placeholder:text-light-silver 
           focus:border-[#0071e3] focus:outline focus:outline-[1px] focus:outline-[#0071e3]
           peer pb-[0px] ${
-            errors?.[label] &&
-            "!border-dark-red text-dark-red bg-light-red active:!border-dark-red focus:!border-dark-red"
-          }`}
+          errors?.[label] &&
+          "!border-dark-red text-dark-red bg-light-red active:!border-dark-red focus:!border-dark-red focus:!outline-none"
+        }`}
         placeholder={" "}
         value={fixedValue}
         data-filled={fixedValue ? "true" : "false"}
@@ -57,9 +57,13 @@ const InputField = ({
       />
       <label
         htmlFor={id}
-        className={`absolute left-4 transition-all duration-200 ease-in-out text-[17px] text-light-silver peer-focus:top-4 peer-focus:text-[10px] peer-focus:text-blue-500 top-1/2 -translate-y-1/2 peer-data-[filled=true]:top-4 peer-data-[filled=true]:text-[10px] ${
-          errors?.[label] && "!text-dark-red"
-        }`}
+        className={`absolute left-4 transition-all duration-200 ease-in-out 
+          text-[17px] text-light-silver 
+          peer-focus:top-4 peer-focus:text-[10px] peer-focus:text-blue-500
+          top-1/2 -translate-y-1/2
+          peer-data-[filled=true]:top-4
+          peer-data-[filled=true]:text-[10px]
+          ${errors?.[label] && "!text-dark-red"}`}
         data-filled="false"
       >
         {placeholder}
